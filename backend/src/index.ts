f76308578825a8490d9642a606159c98976e9579
George Daniel <georgeabiamakadaniel@gmail.com>
2025-12-04 23:04:59 +0100

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,52 @@
-// backend/src/index.ts
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import { keyRouter } from './routers/keyRouter';
-import { orderRouter } from './routers/orderRouter';
-import { productRouter } from './routers/productRouter';
-import { seedRouter } from './routers/seedRouter';
-import { userRouter } from './routers/userRouter';
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
+import path from 'path'
+import { keyRouter } from './routers/keyRouter'
+import { orderRouter } from './routers/orderRouter'
+import { productRouter } from './routers/productRouter'
+import { seedRouter } from './routers/seedRouter'
+import { userRouter } from './routers/userRouter'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/tsmernamazonadb'
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(() => {
+    console.log('error mongodb')
+  })
 
-// MongoDB Connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/tsmernamazonadb';
-mongoose.set('strictQuery', true);
-mongoose.connect(MONGODB_URI).then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.log('MongoDB connection error:', err));
+const app = express()
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  })
+)
 
-// Middleware
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:5173'],
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// Routes
-app.use('/api/products', productRouter);
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
-app.use('/api/seed', seedRouter);
-app.use('/api/keys', keyRouter);
+app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
+app.use('/api/orders', orderRouter)
+app.use('/api/seed', seedRouter)
+app.use('/api/keys', keyRouter)
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')))
+app.get('*', (req: Request, res: Response) =>
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'))
+)
+
+const PORT: number = parseInt((process.env.PORT || '4000') as string, 10)
+
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`)
+})
